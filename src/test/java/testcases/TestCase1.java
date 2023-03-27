93b1e31acb66bf250e2ecde4449cde17781c3251
rounakaftab <rounak.aftab@gmail.com>
2023-03-27 22:41:33 +0600

    Project commit first

---
+++ src/test/java/testcases/TestCase1.java
@@ -0,0 +1,21 @@
+package testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	
+	
+	@Test
+public void practice1() {
+		WebDriver driver;
+		ChromeOptions co = new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+		driver = new ChromeDriver(co);
+		driver.get("https://automationtesting.co.uk/");
+		
+		
+	}
+}
